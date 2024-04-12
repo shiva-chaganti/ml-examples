b46b8439b774fff36c8ac5fd81dadb088f96db91
shiva-chaganti <stchaganti@gmail.com>
2024-04-12 07:31:51 +0100

    Applied batch norm to improve performance

--- pytorch/cat_detector/dnn_model.py
+++ pytorch/cat_detector/dnn_model.py
@@ -9,15 +9,18 @@ class CatDetectorDNN(nn.Module):
     def __init__(self, input_size):
         super(CatDetectorDNN, self).__init__()
         self.fc1 = nn.Linear(input_size, 4096)
+        self.bn1 = nn.BatchNorm1d(4096)
         self.fc2 = nn.Linear(4096, 1024)
+        self.bn2 = nn.BatchNorm1d(1024)
         self.fc3 = nn.Linear(1024, 256)
+        self.bn3 = nn.BatchNorm1d(256)
         self.fc4 = nn.Linear(256, 1)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
-        x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = torch.tanh(self.fc3(x))
+        x = torch.tanh(self.bn1(self.fc1(x)))
+        x = torch.tanh(self.bn2(self.fc2(x)))
+        x = torch.tanh(self.bn3(self.fc3(x)))
         x = torch.sigmoid(self.fc4(x))
         return x
 
