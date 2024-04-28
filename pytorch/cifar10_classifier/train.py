03e4fd0b21f59f83e2d837545add0eedeb93ed51
shiva-chaganti <stchaganti@gmail.com>
2024-04-28 11:12:37 +0100

    Fined tuned classifier for alexnet on cifar10 data

--- pytorch/cifar10_classifier/train.py
+++ pytorch/cifar10_classifier/train.py
@@ -1,7 +1,7 @@
 from functools import partial
 import torch
 from torchsummary import summary
-from alex_net_model import alexnet_model, train_alexnet, evaluate_model
+from alex_net_model import FinetunedAlexNet, train, evaluate_model
 from load_data import get_cifar10_dataset, get_data_loader, transforms_for_alexnet
 
 
@@ -15,10 +15,10 @@ def main():
     get_dataset_fn = partial(get_cifar10_dataset, alexnet_data_transform, data_download_path)
     train_loader, test_loader = get_data_loader(get_datasets_fn=get_dataset_fn(), batch_size=batch_size)
 
-    model = alexnet_model(num_classes)
+    model = FinetunedAlexNet(num_classes)
     print("Summary: \n")
     print(f'{summary(model, (3, 255, 255))}')
-    train_alexnet(model=model, num_epochs=10, train_loader=train_loader, model_save_path=model_save_path)
+    train(model=model, num_epochs=10, train_loader=train_loader, model_save_path=model_save_path)
     model.load_state_dict(torch.load(model_save_path))
     evaluate_model(model=model, test_loader=test_loader)
 
